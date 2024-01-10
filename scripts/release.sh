cc46066f52a0cc83c954c0c37c0b58e611146af1
Di Zou <di.zou1214@gmail.com>
2024-01-10 18:04:34 -0500

    feat: autorelease

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,11 +1,16 @@
 set -e
 wails build --platform darwin/universal
 npx --yes create-dmg build/bin/yazu.app build/bin --overwrite || true
-version=$(git describe --tags `git rev-list --tags --max-count=1`)
+
+version=$(npm version $@) # Adjust as needed
+git tag version
+
 rm -rf build/bin/yazu_*.dmg
 mv build/bin/yazu\ 1.0.0.dmg build/bin/yazu_${version}.dmg
 
 conventional-changelog -p angular -i CHANGELOG.md -s
+
+git commit -am "Update changelog"
 
 npx --yes gh-release \
   --assets build/bin/yazu_${version}.dmg \
@@ -15,3 +20,4 @@ npx --yes gh-release \
   -n ${version} \
   -o xairline \
   -r yet-anther-zibo-updater
+git push && git push --tags
