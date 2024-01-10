7296442df61527be40e2e2cb17d90d9fe296b40c
Di Zou <di.zou1214@gmail.com>
2024-01-10 18:17:21 -0500

    Update changelog

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,17 +4,14 @@ set -e
 #wails build --platform darwin/universal
 
 npx --yes create-dmg build/bin/yazu.app build/bin --overwrite || true
-
 version=$(npm version $@ --no-git-tag-version) # Adjust as needed
-git tag ${version}
-
 rm -rf build/bin/yazu_*.dmg
 mv build/bin/yazu\ 1.0.0.dmg build/bin/yazu_${version}.dmg
 
 conventional-changelog -p angular -i CHANGELOG.md -s
 
 git commit -am "Update changelog"
-
+git tag ${version}
 npx --yes gh-release \
   --assets build/bin/yazu_${version}.dmg \
   -t ${version} \
