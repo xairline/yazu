7879a75610cacbfc5ef7bf92191ea2952fb8b8bb
Di Zou <di.zou1214@gmail.com>
2024-01-10 17:15:11 -0500

    feat(autorelease): add auto update

--- CHANGELOG.md
+++
@@ -1,4 +0,0 @@
-## [0.0.1](https://github.com/xairline/yet-another-zibo-updater/compare/v0.0.1-alpha.18...v0.0.1) (2024-01-10)
-
-
-
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,10 +1,17 @@
 set -e
-wails build --platform darwin/universal
-npx --yes create-dmg build/bin/yazu.app build/bin --overwrite || true
-version=$(git describe --tags `git rev-list --tags --max-count=1`)
-rm -rf build/bin/yazu_*.dmg
-mv build/bin/yazu\ 1.0.0.dmg build/bin/yazu_${version}.dmg
+#wails build --platform darwin/universal
+#npx --yes create-dmg build/bin/yazu.app build/bin --overwrite || true
+#version=$(git describe --tags `git rev-list --tags --max-count=1`)
+#rm -rf build/bin/yazu_*.dmg
+#mv build/bin/yazu\ 1.0.0.dmg build/bin/yazu_${version}.dmg
 
 conventional-changelog -p angular -i CHANGELOG.md -s
 
-npx --yes gh-release --assets build/bin/yazu_${version}.dmg -t ${version} --prerelease -y -c main -n ${version}
+npx --yes gh-release \
+  --assets build/bin/yazu_${version}.dmg \
+  -t ${version} \
+  --prerelease \
+  -y -c main \
+  -n ${version} \
+  -o xairline \
+  -r yet-anther-zibo-updater
