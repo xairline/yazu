013ecbe8bc0a5b97cb7233023b8e8f8e9864291a
Di Zou <di.zou1214@gmail.com>
2024-02-08 08:46:16 -0500

    Update changelog

--- scripts/release.sh
+++ scripts/release.sh
@@ -18,7 +18,7 @@ npx --yes create-dmg@6.1.0 build/bin/yazu.app build/bin --overwrite || true
 mv "build/bin/yazu 1.0.0.dmg" "build/bin/yazu_${version}.dmg"
 mv "build/bin/yazu-amd64.exe" "build/bin/yazu_${version}.exe"
 
-conventional-changelog -p angular -i CHANGELOG.md -s
+conventional-changelog -p angular -i CHANGELOG.md -s || true
 
 git commit -am "Update changelog"
 git tag ${version}
