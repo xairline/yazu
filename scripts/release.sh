65c9d5cf36a1c944bb7d807d16e21d738baa686f
Di Zou <di.zou1214@gmail.com>
2024-02-08 22:56:11 -0500

    ci: fix release version

--- scripts/release.sh
+++ scripts/release.sh
@@ -12,11 +12,11 @@ sed -i '' "s/\"version\": \".*\"/\"version\": \"$version\"/" wails.json
 # Create a Go file with the version
 echo "package main\n\n// AppVersion is the current version of the app\nconst AppVersion = \"$version\"" > appversion.go
 #git commit -am "Bump version to $version"
-#rm -rf build/bin
-#wails build --platform windows/amd64,darwin/universal,linux/amd64
-#npx --yes create-dmg@6.1.0 build/bin/yazu.app build/bin --overwrite || true
-mv "build/bin/yazu_1.0.0.dmg" "build/bin/yazu_${version}.dmg"
-mv "build/bin/yazu_1.0.0.exe" "build/bin/yazu_${version}.exe"
+rm -rf build/bin
+wails build --platform windows/amd64,darwin/universal,linux/amd64
+npx --yes create-dmg@6.1.0 build/bin/yazu.app build/bin --overwrite || true
+mv "build/bin/yazu 1.0.0.dmg" "build/bin/yazu_${version}.dmg"
+mv "build/bin/yazu-amd64.exe" "build/bin/yazu_${version}.exe"
 
 conventional-changelog -p angular -i CHANGELOG.md -s || true
 
